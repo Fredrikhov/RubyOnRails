6023b0b87d7272cd056410541f6a548d1f5bf29f
Andreas Isnes Nilsen <andnil94@hotmail.no>
2018-09-09 13:36:39 +0200

    liten start

--- untitled3/app/controllers/users_controller.rb
+++ untitled3/app/controllers/users_controller.rb
@@ -13,13 +13,12 @@ class UsersController < ApplicationController
     if @user.save
       session[:user_id] = @user.id
       redirect_to '/'
-      flash[:danger] = "Velkommen tilbake"
+      flash[:success] = "Velkommen tilbake"
     else
-      flash[:danger] = "Felter kan ikke være tomme"
+      flash[:fields] = "Felter kan ikke være tomme"
+      redirect_to '/'
     end
   end
-
-  private
 
   def user_params
     params.require(:user).permit(:email, :password, :password_conf)
