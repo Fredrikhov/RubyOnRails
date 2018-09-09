1d0dadcbb38fcfceb4c53df9a69b4647766f2ef9
Andreas Isnes Nilsen <andnil94@hotmail.no>
2018-09-09 12:20:08 +0200

    added database env

--- untitled3/db/schema.rb
+++ untitled3/db/schema.rb
@@ -12,9 +12,6 @@
 
 ActiveRecord::Schema.define(version: 20180830182910) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "users", force: :cascade do |t|
     t.string "email"
     t.string "password_digest"
